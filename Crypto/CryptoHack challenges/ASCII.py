07a1e88549de945451b773fe6fcd5cd47296122d
Maher Sobieh <maher.sobieh.721@gmail.com>
2025-07-02 11:36:36 +0300

    Add Python solution for CryptoHack challenge
    This script solves the "ASCII" challenge from CryptoHack.

---
+++ Crypto/CryptoHack challenges/ASCII.py
@@ -0,0 +1,7 @@
+numbers = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
+
+flag = ""
+for num in numbers:
+    flag += chr(num)
+
+print("The flag is:", flag)
